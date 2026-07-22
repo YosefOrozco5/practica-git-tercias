b93c429094a7229ec10b42ea82c2c030607c047c
Raymond Antonio Dueñas Ibarra <raymond.ad.ibarra@gmail.com>
2026-07-21 22:10:15 -0600

    conflicto c

--- matematicas.py
+++ matematicas.py
@@ -1,7 +1,8 @@
 # Proyecto Colaborativo - Tercia
 numeros = [12, 45, 7, 23, 64, 10, 89, 3]
 
-print("Resultados del analisis estadistico:")
+print("Resultados del analisis de Estudiante C:")
+
 
 promedio = sum(numeros) / len(numeros)
 print(f"El promedio de la lista es: {promedio}")
