290f14c9bc6290b2aafc1a77da1f32136b761c9d
Nelson Uriel Gutierrez Nava <2025310191@uteq.edu.mx>
2026-07-21 22:27:28 -0600

    Solucion conflicto C

--- matematicas.py
+++ matematicas.py
@@ -1,7 +1,7 @@
 # Proyecto Colaborativo - Tercia
 numeros = [12, 45, 7, 23, 64, 10, 89, 3]
 
-print("Resultados del analisis estadistico:")
+print("Resultados del analisis de Estudiante B:")
 
 promedio = sum(numeros) / len(numeros)
 print(f"El promedio de la lista es: {promedio}")
