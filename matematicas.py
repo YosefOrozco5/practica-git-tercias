4cb6ce534eb987fe515acabb5a8cae7539f0d6cb
Raymond Antonio Dueñas Ibarra <raymond.ad.ibarra@gmail.com>
2026-07-21 21:32:09 -0600

    Añadi la funcion de que busque el numero mayor y menor de una lista de numeros

--- matematicas.py
+++ matematicas.py
@@ -2,3 +2,8 @@
 numeros = [12, 45, 7, 23, 64, 10, 89, 3]
 
 print("Resultados del analisis estadistico:")
+
+# Desarrollado por Estudiante C
+maximo = max(numeros)
+minimo = min(numeros)
+print(f"Maximo: {maximo} | Minimo: {minimo}")
