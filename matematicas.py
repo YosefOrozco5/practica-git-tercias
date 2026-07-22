785204c8c6c4364d2457bb134e74ddca975a5ba3
Nelson Uriel Gutierrez Nava <2025310191@uteq.edu.mx>
2026-07-21 22:23:37 -0600

    Conflicto B

--- matematicas.py
+++ matematicas.py
@@ -3,7 +3,6 @@ numeros = [12, 45, 7, 23, 64, 10, 89, 3]
 
 print("Resultados del analisis de Estudiante B:")
 
-
 promedio = sum(numeros) / len(numeros)
 print(f"El promedio de la lista es: {promedio}")
 # Desarrollado por Estudiante C
